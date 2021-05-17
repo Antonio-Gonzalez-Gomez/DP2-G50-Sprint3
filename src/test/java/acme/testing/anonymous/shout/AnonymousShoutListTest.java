9e0af2fd8500c4631773a8ab93c8def5d72aa05f
Beatriz Beltrán Álvarez <beltranalvarezbeatriz@gmail.com>
2021-05-17 14:22:12 +0200

    Finished Shout list test

---
+++ src/test/java/acme/testing/anonymous/shout/AnonymousShoutListTest.java
@@ -0,0 +1,27 @@
+package acme.testing.anonymous.shout;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
+import org.springframework.core.annotation.Order;
+
+import acme.testing.AcmeOneTest;
+
+public class AnonymousShoutListTest extends AcmeOneTest{
+	@ParameterizedTest
+	@CsvFileSource(resources="/anonymous/shout/list-shout.csv", encoding = "utf-8", numLinesToSkip=1)
+	@Order(10)
+	public void listShout(final int recordIndex, final String moment, final String author, final String text) {
+
+		super.clickOnMenu("Anonymous", "List Shouts");
+		
+		super.checkColumnHasValue(recordIndex, 0, moment);
+		super.checkColumnHasValue(recordIndex, 1, author);
+		super.checkColumnHasValue(recordIndex, 2, text);
+		
+		super.checkInputBoxHasValue("Moment", moment);
+		super.checkInputBoxHasValue("Author", author);		
+		super.checkInputBoxHasValue("Statement", text);
+		
+		super.signOut();
+	}
+}
