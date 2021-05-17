503ab1a63f79e6a29744466a7108ccaed69fd6a7
Beatriz Beltrán Álvarez <beltranalvarezbeatriz@gmail.com>
2021-05-17 16:33:02 +0200

    Finished creating tests corresponding to the positive case for creating shouts

---
+++ src/test/java/acme/testing/anonymous/shout/AnonymousShoutCreateTest.java
@@ -0,0 +1,26 @@
+package acme.testing.anonymous.shout;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
+import org.springframework.core.annotation.Order;
+
+import acme.testing.AcmeOneTest;
+
+public class AnonymousShoutCreateTest extends AcmeOneTest{
+	@ParameterizedTest
+	@CsvFileSource(resources="/anonymous/shout/create-positive-shout.csv", encoding = "utf-8", numLinesToSkip=1)
+	@Order(10)
+	public void createPositiveShout(final int recordIndex, final String moment, final String author, final String text) {
+
+		super.clickOnMenu("Anonymous", "Create Shouts");
+		
+		super.fillInputBoxIn("author", author);
+		super.fillInputBoxIn("text", text);
+		super.clickOnSubmitButton("Create");
+		
+		super.clickOnMenu("Anonymous", "List Shouts");
+		
+		super.checkColumnHasValue(recordIndex, 1, author);
+		super.checkColumnHasValue(recordIndex, 2, text);
+	}
+}
